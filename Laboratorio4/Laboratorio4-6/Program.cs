525d131725e40d8a27d96c731ecec09823c42825
GAB507 <gabriel.cubilla@utp.ac.pa>
2024-09-10 19:33:10 -0500

    ultimo commit , terminando Laboratorio-4

---
+++ Laboratorio4/Laboratorio4-6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite el radio del circulo ");
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = Math.Pow(radio, 2);
+
+            Console.WriteLine($"El area del circulo es:  {area}");
+             
+        }
+    }
+}
